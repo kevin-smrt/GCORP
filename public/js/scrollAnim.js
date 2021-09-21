95701a2d44312fcb02626c9f93c0977d694773a1
kevin_smrt <kevinsanmartin@live.fr>
2021-09-21 12:36:02 +0200

    remove scroll on profile picture and change offset limit to galeries

--- public/js/scrollAnim.js
+++ public/js/scrollAnim.js
@@ -1,7 +1,6 @@
 // Récupère la photo
 const profilPicture = document.querySelector('.intro__img');
-const headerIntro = document.querySelector('.header__intro');
-const mainIntro = document.querySelector('.main__intro');
+const headerIntro = document.querySelector('.intro__about');
 const galeries = document.querySelector('.galeries');
 
 // Ajout d'un événement sur le la fenêtre de type scroll
@@ -9,22 +8,10 @@ window.addEventListener('scroll', () => {
     // Prend la valeur qui correspond au scroll sur l'axe Y
     let offset = window.scrollY;
 
-    // Si la taille de la fenêtre est supérieur a 1200px
-    if (window.innerWidth > 1200) {
-        // Déplace la photo selon la valeur du scroll
-        profilPicture.style.top = `${offset * 0.1}px`
-        profilPicture.style.left = `${offset * 0.08}px`
-    } else { // Si l'utilisateur change la taille manuellement, il peut y avoir un problème de position, donc l'image est reset a sa position de base
-        profilPicture.style.top = `0px`
-        profilPicture.style.left = `0px`
-    }
-
     // Si offset est supérieur à 100, ajoute la classe slide_left, sinon la retire
     offset > 100 ? profilPicture.classList.add('slide_left') : profilPicture.classList.remove('slide_left');
 
-    offset > 100 ? headerIntro.classList.add('slide_left') : headerIntro.classList.remove('slide_left');
+    offset > 100 ? headerIntro.classList.add('slide_right') : headerIntro.classList.remove('slide_right');
 
-    offset > 600 ? mainIntro.classList.add('slide_right') : mainIntro.classList.remove('slide_right');
-
-    offset > 1100 ? galeries.classList.add('slide_bot') : galeries.classList.remove('slide_bot');
+    offset > 700 ? galeries.classList.add('slide_bot') : galeries.classList.remove('slide_bot');
 });
