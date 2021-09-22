1993de155c4100ad3ac1a5572a883e21d53d9fc1
kevin_smrt <kevinsanmartin@live.fr>
2021-09-22 13:29:34 +0200

    add lazy loading to footer insta img

---
+++ public/js/footerLazyLoading.js
@@ -0,0 +1,22 @@
+// Récupère les éléments qui vont s'activer au scroll
+const instaPictures = document.querySelector('.footer__insta');
+
+// Création d'une instance de l'API intersection observer
+const footerObserver = new IntersectionObserver((entries, observer) => {
+    // Pour chaque élément obsérvé
+    entries.forEach((entry) => {
+        // Si l'élément rentre dans le viewport
+        if (entry.isIntersecting) {
+            entry.target.querySelectorAll('img').forEach((img) => {
+                img.src = img.dataset.src;
+            });
+        }
+    })
+}, {
+    // Definit le pourcentage de visibilité pour déclenché l'action
+    threshold: 0.01
+});
+
+// Elements que l'API obeserve
+footerObserver.observe(instaPictures);
+
