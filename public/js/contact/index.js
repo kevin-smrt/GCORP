7a6fd390bca2ee84893ca0b48c2f4a479f35789e
kevin_smrt <kevinsanmartin@live.fr>
2021-09-27 09:41:09 +0200

    add commentaries

--- public/js/contact/index.js
+++ public/js/contact/index.js
@@ -1,12 +1,17 @@
+// Récupère les éléments qui servent à afficher le formulaire de mariage
 const presta = document.querySelector('#presta');
 const formMariage = document.querySelector('.form__mariage');
 
 function showMariageForm() {
+    // Si l'élément select a la valeur mariage
     if (presta.value == "mariage") {
+        // Ajoute la valeur flex au formulaire de mariage
         formMariage.style.display = "flex";
     } else {
+        // Sinon, le cache
         formMariage.style.display = "none";
     }
 }
 
+// Ajout de l'écouteur d'evenement sur le bouton select
 presta.addEventListener('click', showMariageForm);
