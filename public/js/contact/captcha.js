bd8d0bbaa439df54f69851d83847d150371555d8
kevin_smrt <kevinsanmartin@live.fr>
2021-10-07 11:57:22 +0200

    test captcha

---
+++ public/js/contact/captcha.js
@@ -0,0 +1,6 @@
+const contactForm = document.querySelector('.form-container');
+const captcha = document.querySelector('g-recaptcha');
+
+contactForm.addEventListener('submit', () => {
+    console.log(captcha);
+});
